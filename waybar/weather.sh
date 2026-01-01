ea4d741a7c18e2072f12705ce28c76edb786ce0a
Inshaal2003 <badcoderinshaal03@gmail.com>
2026-01-01 22:53:39 +0500

    .

--- waybar/weather.sh
+++ waybar/weather.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
-for i in {1..5}
-do
+
+for i in {1..5}; do
     text=$(curl -s "https://wttr.in/$1?format=1")
-    if [[ $? == 0 ]]
-    then
-        text=$(echo "$text" | sed -E "s/\s+/ /g")
+    if [[ $? == 0 ]]; then
+        text=$(echo "$text" \
+            | sed -E 's/^([^0-9+-]*)(.*)$/\2 \1/' \
+            | sed -E 's/\s+/ /g; s/ $//')
         tooltip=$(curl -s "https://wttr.in/$1?format=4")
-        if [[ $? == 0 ]]
-        then
-            tooltip=$(echo "$tooltip" | sed -E "s/\s+/ /g")
+        if [[ $? == 0 ]]; then
+            tooltip=$(echo "$tooltip" | sed -E 's/\s+/ /g')
             echo "{\"text\":\"$text\", \"tooltip\":\"$tooltip\"}"
             exit
         fi
     fi
     sleep 2
 done
+
 echo "{\"text\":\"error\", \"tooltip\":\"error\"}"
